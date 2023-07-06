6339afb7efe2d3f03e9a438db94cec70b9779e2c
Rami Bankale <ramibankale@Ramis-MBP.attlocal.net>
2023-07-06 13:18:07 -0500

    adding show route and function

--- models/skill.js
+++ models/skill.js
@@ -5,9 +5,16 @@ const skills = [
 ];
 	
 module.exports = {
-    getAll
+    getAll,
+    getOne
 };
 	
+function getOne(id) {
+    id = parseInt(id);
+    return skills.find(skill => skill.id === id);
+
+}
+
 function getAll() {
     return skills;
 }
--- routes/skills.js
+++ routes/skills.js
@@ -4,7 +4,9 @@ var skillsCtrl = require('../controllers/skills');
 
 // All actual paths start with "/skills"
 
-
+// Get /skills
 router.get('/', skillsCtrl.index);
+// Get /skills/:id
+router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
