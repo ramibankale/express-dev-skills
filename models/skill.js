169ec009978a2a046fe195698086da3f1266001c
Rami Bankale <ramibankale@Ramis-MBP.attlocal.net>
2023-07-05 22:49:25 -0500

    Added Skill.js

---
+++ models/skill.js
@@ -0,0 +1,13 @@
+const todos = [
+    {id: 125223, skill: 'HyperText Markup Language HTML', have: true},
+    {id: 127904, skill: 'Cascading Style Sheets CSS', have: false},
+    {id: 139608, skill: 'JavaScript', have: false}
+];
+	
+module.exports = {
+    getAll
+};
+	
+function getAll() {
+    return skills;
+}
--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// The first argumenr in the "start with" path
+// The path within the route modules are appended
+// to the start with paths
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
